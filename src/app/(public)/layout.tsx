cf809255ae9959146c3c9d0984db08fa31ac3de4
guilhermeEloi <gui.eloi.silva@gmail.com>
2025-06-14 13:38:54 -0300

    fix fonts

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,16 +1,10 @@
 "use client";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
-import { Space_Grotesk } from "next/font/google";
+import { spaceGrotesk } from "@/styles/fonts/fonts";
 
 import "@/styles/globals.css";
 import "keen-slider/keen-slider.min.css";
-
-export const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  display: "swap",
-});
 
 export default function RootLayout({
   children,
